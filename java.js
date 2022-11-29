81c9d3f759db7562ef1c0c0a4081b2586ec0de0c
blockkhammar <blockhammar.programmering@gmail.com>
2022-11-29 13:15:11 +0100

    Skapa deras bransh

--- java.js
+++ java.js
@@ -1,7 +1,14 @@
+
+
 function doStuff(input){
-	if (document.getElementById(input).src == "https://toppng.com/uploads/preview/light-bulb-on-off-png-115539402943y50vxr5yi.png"){
-        document.getElementById(input).src = "https://img.freepik.com/free-vector/realistic-light-bulb-with-electricity_23-2149129410.jpg";
-    } else {
-        document.getElementById(input).src = "https://toppng.com/uploads/preview/light-bulb-on-off-png-115539402943y50vxr5yi.png";
+    console.log(document.getElementById(input).src)
+	if (document.getElementById(input).src == "http://127.0.0.1:5500/images/on.png")
+    {
+        document.getElementById(input).src = "images/off.png";
+    } 
+    else {
+
+        document.getElementById(input).src = "images/on.png";
+
     }
 }
