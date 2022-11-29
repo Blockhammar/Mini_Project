0f5ed00bbc620936b6cee2c4cffe875b0a239b2a
Adna Stupar <aasturp@gmail.com>
2022-11-29 11:21:09 +0100

    More code

---
+++ java.js
@@ -0,0 +1,7 @@
+function doStuff(input){
+	if (document.getElementById(input).src == "https://toppng.com/uploads/preview/light-bulb-on-off-png-115539402943y50vxr5yi.png"){
+        document.getElementById(input).src = "https://img.freepik.com/free-vector/realistic-light-bulb-with-electricity_23-2149129410.jpg";
+    } else {
+        document.getElementById(input).src = "https://toppng.com/uploads/preview/light-bulb-on-off-png-115539402943y50vxr5yi.png";
+    }
+}
